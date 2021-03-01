1698ed4a5af971be3f5dac3c66c20d37014d28b5
ahnsoheee <soheeeahn@gmail.com>
2021-03-01 19:25:20 +0900

    env: 개발환경 셋팅

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,35 +1,39 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/app.jsx",
-    output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: "bundle.js",
-    },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            use: 'babel-loader',
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.(scss|css)$/,
-            use: ["style-loader", "css-loader"],
-        }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, './dist'),
-        inline: true,
-        hot: true,
-        port: 3030
-    },
+  mode: "development",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./dist"),
+    inline: true,
+    hot: true,
+    port: 3030,
+  },
 
-    plugins: [new HtmlWebpackPlugin(
-        {
-            template: 'public/index.html'
-        }
-    )]
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
