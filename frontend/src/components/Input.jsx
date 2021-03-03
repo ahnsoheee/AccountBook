e66f3812e5576840c4c7c71fd935080023c2c5ad
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 04:02:16 +0900

    [fe] feat: 입력 컴포넌트 props 추가

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
-const Input = ({ type, name, placeholder }) => {
-  return <InputBox type={type} name={name} placeholder={placeholder} />;
+const Input = ({ type, name, placeholder, value, onChange }) => {
+  return <InputBox type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} />;
 };
 
 const InputBox = styled.input`
