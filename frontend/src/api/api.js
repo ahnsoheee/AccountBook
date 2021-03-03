b8c1e1a9f42fe348ebd99d739a617d0730c66b46
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 03:52:56 +0900

    [fe] feat: fetch 클래스 생성

---
+++ frontend/src/api/api.js
@@ -0,0 +1,19 @@
+class fetchAPI {
+  post(uri, body) {
+    return fetch(uri, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    }).then((res) => res.json());
+  }
+
+  get(uri) {
+    return fetch(uri, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => res.json());
+  }
+}
+
+const API = new fetchAPI();
+export { API };
