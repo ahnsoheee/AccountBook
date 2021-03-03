b788b1ce769510e89846588751c36b4487166779
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 04:01:46 +0900

    [be] feat: user 라우터 추가

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userRouter = require("./api/user");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use("/user", userRouter);
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
 module.exports = router;
