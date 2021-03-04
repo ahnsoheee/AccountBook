b2932a50a48ff705472ab26f5af8a7d19d282038
ahnsoheee <soheeeahn@gmail.com>
2021-03-05 03:17:07 +0900

    [be] feat: 회원가입시 비밀번호 암호화하여 저장

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const userModel = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 class UserService {
   async signup(user) {
     let result;
+    const salt = await bcrypt.genSalt(10);
+    const password = bcrypt.hashSync(user.pw, salt);
     try {
-      result = await userModel.create(user);
+      result = await userModel.create({ id: user.id, pw: password, name: user.name });
     } catch (err) {
       return false;
     }
@@ -16,10 +20,20 @@ class UserService {
     let result;
     try {
       result = await userModel.findUser(user);
+      if (result) {
+        const token = jwt.sign(
+          {
+            id: user.id,
+          },
+          process.env.SECRET_KEY
+        );
+        return token;
+      } else {
+        return false;
+      }
     } catch (err) {
       return false;
     }
-    return true;
   }
 }
 
