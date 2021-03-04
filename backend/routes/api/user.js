e1c655fda2a5fbcee4421c28763cd8786cf969bb
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 17:27:00 +0900

    [be] feat: 로그인 api 추가

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../../controllers/user");
 
 router.post("/signup", controller.signup);
+router.post("/signin", controller.signin);
 
 module.exports = router;
