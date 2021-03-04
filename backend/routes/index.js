d8427dcddbaa1c5646c56dbe075ba0c4421e44fd
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 17:26:21 +0900

    [be] chore

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userRouter = require("./api/user");
 
 router.use("/user", userRouter);
+
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
