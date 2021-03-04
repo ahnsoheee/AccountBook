cfb85d0f98101b97f29bd831cbcc9d0ffe2ad10a
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 17:29:31 +0900

    [fe] feat: App 구성 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import SignPage from "./pages/SignPage";
 import MainPage from "./pages/MainPage";
 
 const App = () => {
-  const [isLogin, setLogin] = useState(false);
-
-  return isLogin ? <MainPage /> : <SignPage />;
+  return <MainPage />;
 };
 
 export default App;
