8e0776643d12c53ccd308562a8afb7bde0b8ad1d
ahnsoheee <soheeeahn@gmail.com>
2021-03-04 17:25:01 +0900

    [be] feat: user 컨트롤러 - 로그인 기능 구현

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,3 +6,9 @@ exports.signup = async (req, res, next) => {
   const result = await userService.signup(userDTO);
   res.json({ result: result });
 };
+
+exports.signin = async (req, res, next) => {
+  const userDTO = req.body;
+  const result = await userService.signin(userDTO);
+  res.json({ result: result });
+};
