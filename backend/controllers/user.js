92c1d52eb789fa8ad45aae98796cb643613d6129
ahnsoheee <soheeeahn@gmail.com>
2021-03-05 20:20:54 +0900

    [be] user 컨트롤러 - 로그아웃 기능 구현

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,23 +3,48 @@ const userService = require("../services/user");
 
 exports.signup = async (req, res, next) => {
   const userDTO = req.body;
-  const result = await userService.signup(userDTO);
-  res.json({ result: result });
+  try {
+    const result = await userService.signup(userDTO);
+    res.json({ result: result });
+  } catch (err) {
+    console.error(err);
+    res.json({ result: false });
+  }
 };
 
 exports.signin = async (req, res, next) => {
   const userDTO = req.body;
-  const token = await userService.signin(userDTO);
-  if (token) {
-    res.cookie("token", token, { httpOnly: true });
+  try {
+    const token = await userService.signin(userDTO);
+    if (token) {
+      res.cookie("token", token, { httpOnly: true });
+    }
+    res.json({ result: token });
+  } catch (err) {
+    console.log(err);
+    res.json({ result: false });
   }
-  res.json({ result: token });
 };
 
 exports.auth = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (token) {
-    const user = await userService.auth(token);
-    res.json({ result: user });
+  try {
+    const token = req.cookies.token;
+    if (token) {
+      const user = await userService.auth(token);
+      res.json(user);
+    }
+  } catch (err) {
+    console.error(err);
+    res.json({ result: false });
+  }
+};
+
+exports.logout = async (req, res, next) => {
+  try {
+    res.clearCookie("token");
+    res.json({ result: true });
+  } catch (err) {
+    console.error(err);
+    res.json({ result: false });
   }
 };
