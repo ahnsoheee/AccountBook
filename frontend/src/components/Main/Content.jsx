8db9b7a01fb5f910232a79a2a8b3a22ad7366eba
ahnsoheee <soheeeahn@gmail.com>
2021-03-05 22:21:18 +0900

    [fe] 메인 페이지 내용 컴포넌트 구현

---
+++ frontend/src/components/Main/Content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Content = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+const Wrapper = styled.div``;
+
+export default Content;
