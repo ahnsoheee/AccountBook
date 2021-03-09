e22880bb6cd18c60e2148da116d360a79cb20c03
ahnsoheee <soheeeahn@gmail.com>
2021-03-10 00:55:14 +0900

    [be] feat: log 서비스 구현

---
+++ backend/services/log.js
@@ -0,0 +1,35 @@
+const logModel = require("../models/log");
+class LogService {
+  async read(user) {
+    let result;
+    try {
+      result = await logModel.findById(user.id);
+    } catch (err) {
+      return false;
+    }
+    return result;
+  }
+
+  // async create(log) {
+  //   let result;
+  //   try {
+  //     result = await logModel.add(log);
+  //   } catch (err) {
+  //     return false;
+  //   }
+  //   return result;
+  // }
+
+  // async delete(log) {
+  //   let result;
+  //   try {
+  //     result = await logModel.delete(log.id);
+  //   } catch (err) {
+  //     return false;
+  //   }
+  //   return result;
+  // }
+}
+
+const logService = new LogService();
+module.exports = logService;
