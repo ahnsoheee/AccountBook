2d349e0c3246ef97d9f7807fca1ee800e40ff376
ahnsoheee <soheeeahn@gmail.com>
2021-03-09 13:59:12 +0900

    [fe] feat: 내역 입력 폼 구현

--- frontend/src/components/Main/Input.jsx
+++ frontend/src/components/Main/Input.jsx
@@ -4,6 +4,11 @@ import styled from "styled-components";
 const Input = () => {
   const [income, setIncome] = useState(true);
   const [expend, setExpend] = useState(false);
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [account, setAccount] = useState("");
+  const [cost, setCost] = useState("");
+  const [content, setContent] = useState("");
 
   const onClickIncome = () => {
     setIncome(true);
@@ -13,6 +18,26 @@ const Input = () => {
   const onClickExpend = () => {
     setIncome(false);
     setExpend(true);
+  };
+
+  const onChangeDate = (e) => {
+    setDate(e.target.value);
+  };
+
+  const onChangeCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const onChangeAccount = (e) => {
+    setAccount(e.target.value);
+  };
+
+  const onChangeCost = (e) => {
+    setCost(e.target.value);
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
   };
 
   return (
@@ -29,15 +54,25 @@ const Input = () => {
       <Items>
         <Div>
           <Name>날짜</Name>
-          <Date type="date" />
+          <Date type="date" onChange={onChangeDate} />
         </Div>
         <Div>
           <Name>카테고리</Name>
-          <Select />
+          <Select onChange={onChangeCategory} />
         </Div>
         <Div>
           <Name>결제수단</Name>
-          <Select />
+          <Select onChange={onChangeAccount} />
+        </Div>
+      </Items>
+      <Items>
+        <Div>
+          <Name>금액</Name>
+          <Content type="text" onChange={onChangeCost} />
+        </Div>
+        <Div>
+          <Name>내용</Name>
+          <Content type="text" onChange={onChangeContent} />
         </Div>
       </Items>
     </Wrapper>
@@ -75,7 +110,6 @@ const Button = styled.button`
 
 const Items = styled.div`
   display: flex;
-  flex-grow: 1;
   justify-content: center;
   margin: 10px;
 `;
@@ -83,6 +117,7 @@ const Items = styled.div`
 const Div = styled.div`
   display: flex;
   width: 200px;
+  flex-grow: 1;
 `;
 
 const Date = styled.input`
@@ -94,6 +129,14 @@ const Date = styled.input`
 const Select = styled.select`
   width: 120px;
   margin-left: 10px;
+`;
+
+const Content = styled.input`
+  width: 235px;
+  text-align: center;
+  margin-left: 10px;
+  border: none;
+  outline: none;
 `;
 
 export default Input;
