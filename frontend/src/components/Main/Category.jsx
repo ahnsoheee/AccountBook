6a9d3dd8e5c8c73f96ee700a6f24de8c1550062b
ahnsoheee <soheeeahn@gmail.com>
2021-03-10 18:22:33 +0900

    [fe] feat: 카테고리 선택 컴포넌트 구현

---
+++ frontend/src/components/Main/Category.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const Category = ({ categories, onClick }) => {
+  if (categories) {
+    const category = categories.map((category) => {
+      return (
+        <option value={category.id} key={category.id}>
+          {category.name}
+        </option>
+      );
+    });
+    return (
+      <Select onClick={onClick}>
+        <option value="none">선택하세요</option>
+        {category}
+      </Select>
+    );
+  }
+  return <Select />;
+};
+
+const Select = styled.select`
+  width: 120px;
+  margin-left: 10px;
+  border: none;
+  border-bottom: 1px solid #cccccc;
+  background-color: #f8f1f1;
+  outline: none;
+`;
+
+export default Category;
