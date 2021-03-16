3f3633962b7c5f9f3b55799462ea048cc353964c
ahnsoheee <soheeeahn@gmail.com>
2021-03-17 01:04:57 +0900

    [fe] feat: 카테고리 항목 컴포넌트 구현

---
+++ frontend/src/components/Common/CategoryList.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import { API } from "../../api/api";
+
+const CategoryList = ({ id, name, user, setIncomes, setExpends }) => {
+  const onUpdate = async () => {
+    alert("준비 중 입니다.");
+  };
+
+  const onDelete = async () => {
+    const result = await API.delete("/input/category", { id: id });
+    if (result) {
+      const incomes = await API.post("/input/category", { id: user, type: "수입" });
+      const expends = await API.post("/input/category", { id: user, type: "지출" });
+      setIncomes(incomes);
+      setExpends(expends);
+    }
+  };
+
+  return (
+    <List>
+      <Name>{name}</Name>
+      <UpdateButton onClick={onUpdate}>수정</UpdateButton>
+      <DeleteButton onClick={onDelete}>삭제</DeleteButton>
+    </List>
+  );
+};
+
+const List = styled.div`
+  display: flex;
+  margin: 5px;
+  font-size: 11pt;
+  font-weight: bold;
+`;
+
+const Name = styled.div`
+  width: 370px;
+  padding-left: 80px;
+  color: #000000;
+  text-align: left;
+`;
+
+const UpdateButton = styled.button`
+  width: 50px;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  background-color: #ff4646;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const DeleteButton = styled.button`
+  width: 50px;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  background-color: #ff9292;
+  font-weight: bold;
+  color: #ffffff;
+  margin-left: 10px;
+`;
+
+export default CategoryList;
