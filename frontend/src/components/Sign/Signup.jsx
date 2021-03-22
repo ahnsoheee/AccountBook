760ee7e3a6e8494a5207ea5ab3e8d8a71b031c23
ahnsoheee <soheeeahn@gmail.com>
2021-03-22 18:06:02 +0900

    [fe] feat: 회원가입시 비밀번호 확인 입력 추가

--- frontend/src/components/Sign/Signup.jsx
+++ frontend/src/components/Sign/Signup.jsx
@@ -7,7 +7,8 @@ import Signin from "./Signin";
 
 const Signup = () => {
   const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  const [pw1, setPw1] = useState("");
+  const [pw2, setPw2] = useState("");
   const [name, setName] = useState("");
   const [isComplete, setComplete] = useState(false);
 
@@ -15,8 +16,12 @@ const Signup = () => {
     setId(e.target.value);
   };
 
-  const onChangePw = (e) => {
-    setPw(e.target.value);
+  const onChangePw1 = (e) => {
+    setPw1(e.target.value);
+  };
+
+  const onChangePw2 = (e) => {
+    setPw2(e.target.value);
   };
 
   const onChangeName = (e) => {
@@ -24,15 +29,19 @@ const Signup = () => {
   };
 
   const onClick = async () => {
-    if (!id.length || !pw.length || !name.length) {
+    if (!id.length || !pw1.length || !name.length) {
       alert("아이디와 비밀번호, 이름을 모두 입력하세요.");
     } else {
-      const res = await API.post("/user/signup", { id: id, pw: pw, name: name });
-      if (res.result) {
-        alert("회원가입이 성공적으로 완료되었습니다.");
-        setComplete(true);
+      if (pw1 !== pw2) {
+        alert("비밀번호가 일치하지 않습니다.");
       } else {
-        alert("이미 존재하는 아이디입니다.");
+        const res = await API.post("/user/signup", { id: id, pw: pw1, name: name });
+        if (res.result) {
+          alert("회원가입이 성공적으로 완료되었습니다.");
+          setComplete(true);
+        } else {
+          alert("이미 존재하는 아이디입니다.");
+        }
       }
     }
   };
@@ -44,7 +53,8 @@ const Signup = () => {
       <Form title="회원가입">
         <Input type="text" name="name" placeholder="이름" value={name} onChange={onChangeName} required />
         <Input type="text" name="id" placeholder="아이디" value={id} onChange={onChangeId} required />
-        <Input type="password" name="pw" placeholder="비밀번호" value={pw} onChange={onChangePw} required />
+        <Input type="password" name="pw1" placeholder="비밀번호" value={pw1} onChange={onChangePw1} required />
+        <Input type="password" name="pw2" placeholder="비밀번호 확인" value={pw2} onChange={onChangePw2} required />
         <SignButton name="회원가입" width="507px" height="40px" background="#ff4646" onClick={onClick} />
       </Form>
     </>
